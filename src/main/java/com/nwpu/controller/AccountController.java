50bea8b0feb8144d8042f571ed32f701cd9409e0
K <45218419+linkuan123@users.noreply.github.com>
2019-11-17 21:49:00 +0800

    SpringMVC整合完成

--- src/main/java/com/nwpu/controller/AccountController.java
+++ src/main/java/com/nwpu/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.nwpu.controller;
 
+import com.nwpu.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("/findAll")
     public String findAll(){
         System.out.println("表现层：查询所有");
+        accountService.findAll();
         return "list";
     }
 
