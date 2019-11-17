e19dc50e9f463c5b3e3a480a05cbe051ea43cecb
K <45218419+linkuan123@users.noreply.github.com>
2019-11-17 21:28:20 +0800

    SpringMVC环境的搭建

--- src/main/java/com/nwpu/controller/AccountController.java
+++ src/main/java/com/nwpu/controller/AccountController.java
@@ -1,8 +1,19 @@
 package com.nwpu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+/**
+ * 账户web
+ */
+@Controller
+@RequestMapping("/account")
 public class AccountController {
 
+    @RequestMapping("/findAll")
+    public String findAll(){
+        System.out.println("表现层：查询所有");
+        return "list";
+    }
 
 }
